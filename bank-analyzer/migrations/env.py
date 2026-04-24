c24f255859fa1af227a7423b1ff418e3d1230aa9
Ian Kiyoshi Kobayashi <contato.iankyoo@gmail.com>
2026-04-24 14:49:22 -0300

    chore: suppress unused import warning in alembic env

--- bank-analyzer/migrations/env.py
+++ bank-analyzer/migrations/env.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from bank_analyzer.core.base import Base
 from bank_analyzer.core.config import settings
-from bank_analyzer.models import statement, transaction, user
+from bank_analyzer.models import statement, transaction, user  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
