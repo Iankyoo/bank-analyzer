6661cf364f11d7ceca50228c484f5787ec0fadf9
Ian Kiyoshi Kobayashi <contato.iankyoo@gmail.com>
2026-04-25 14:47:58 -0300

    feat: add statement schemas

---
+++ bank-analyzer/src/bank_analyzer/schemas/statements.py
@@ -0,0 +1,17 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from bank_analyzer.core.enums import Status
+
+
+class StatementCreate(BaseModel):
+    filename: str
+
+
+class StatementPublic(BaseModel):
+    id: uuid.UUID
+    filename: str
+    status: Status
+    uploaded_at: datetime
