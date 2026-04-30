1f8598480fb1d29842eaa416244bceea8bf0745a
Ian Kiyoshi Kobayashi <contato.iankyoo@gmail.com>
2026-04-30 13:06:29 -0300

    test: add decode access token test

--- bank-analyzer/tests/unit/test_security.py
+++ bank-analyzer/tests/unit/test_security.py
@@ -1,8 +1,16 @@
-from bank_analyzer.core.security import get_hashed_password, verify_password
+import pytest
+from fastapi import HTTPException
+
+from bank_analyzer.core.security import (
+    create_access_token,
+    decode_access_token,
+    get_hashed_password,
+    verify_password,
+)
 
 
 def test_get_hashed_password():
-    password = 'test'
+    password = "test"
 
     hashed_password = get_hashed_password(password)
 
@@ -12,9 +20,36 @@ def test_get_hashed_password():
 
 
 def test_verify_password():
-    password = 'secret'
+    password = "secret"
 
     hashed_password = get_hashed_password(password)
 
     assert verify_password(password, hashed_password) is True
     assert verify_password("wrong_password", hashed_password) is False
+
+
+def test_create_access_token():
+    sub = {"sub": "username"}
+    token = create_access_token(sub)
+
+    assert token is not None
+    assert isinstance(token, str)
+    assert len(token) > 0
+
+
+def test_decode_access_token():
+    email = "test@email.com"
+
+    token = create_access_token({"sub": email})
+
+    result = decode_access_token(token)
+
+    assert result == email
+
+
+def test_decode_access_token_invalid():
+
+    with pytest.raises(HTTPException) as exc_info:
+        decode_access_token("token_invalido")
+
+    assert exc_info.value.status_code == 401
